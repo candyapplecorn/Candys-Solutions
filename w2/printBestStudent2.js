c3d0961393abd6094b90ce181a5127db73a83af1
Joseph Burger <candyapplecorn@gmail.com>
2016-06-20 23:25:46 -0700

    Added Comments to Explain
    Pretty hard to reason about even with comments

--- w2/printBestStudent2.js
+++ w2/printBestStudent2.js
@@ -20,30 +20,39 @@ IT'S ALIVVVVVE
 */
 function printBestStudent(sts) {
   
+  // Attach each student's name to each element in their grades
   students3.map(
     student => 
       student.grades.map(grades =>
            ({
              id: grades.id, 
              score: grades.score, 
-             name: student.name 
+             name: student.name // Now we have their name in their grades.
            })
       ) 
   )
+  // Get an array of just test score objects
   .reduce((p, c) => p.concat(c), [])
+  // Sort/group them by id
   .sort((a, b) => 
         (a.id < b.id ? -1 : a.id > b.id ? 1 : 0)
-       )
+  )
+  // sub-sort each group by their scores.
   .sort((a, b) => 
        (a.id !== b.id ? 0 :
        a.score > b.score ? -1 : a.score < b.score ? 1 : 0)
   )
+  /* Now the array is ordered by test id; the first
+     three elements will be test id 0; those three
+     elements will be sorted by their scores */
+     
+  // Take only the first (highest score) item for each id/group
   .reduce((p, c) => 
     (p[c.id] === undefined ? (p.push(c), p) : p), []
   )
+  // And finally, print it out!
   .forEach(
-    (e) => 
-    console.log("Test " + e.id + ": " + e.name)
+    (e) => console.log("Test " + e.id + ": " + e.name)
   );
   
 }
